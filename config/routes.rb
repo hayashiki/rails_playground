46f1c9c868cd725d96ad35ad176cd3d4ab6f9644
hayashiki <peperoncino.pop@gmail.com>
2018-10-14 08:27:38 +0900

    Implement session destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'root#index'
   resources :users
 
+  get '/logout', to: 'sessions#destroy'
   get '/auth/google_oauth2/callback' => 'authentication#callback'
 end
